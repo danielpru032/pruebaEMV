3e667a50cf4a5133bab1c53b557ad685f43e0f64
“iver” <correo@gmail.com>
2024-03-15 07:28:22 -0400

    void

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [AppComponent, MatIconModule, MatDividerModule, MatButtonModule],
     }).compileComponents();
   });
 
@@ -24,6 +27,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, prueba');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, prueba'
+    );
   });
 });
